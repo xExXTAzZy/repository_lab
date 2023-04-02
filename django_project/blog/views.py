8270092be66f52cbb2dfd6a82942a977258869dc
xExXTAzZy <95426045+xExXTAzZy@users.noreply.github.com>
2023-04-02 23:21:29 +0300

    Add files via upload

---
+++ django_project/blog/views.py
@@ -0,0 +1,77 @@
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from .models import Post
+from django.urls import reverse_lazy, reverse
+from .forms import CommentForm
+
+
+class PostListView(ListView):
+    model = Post
+    paginate_by = 2
+    template_name = "home.html"
+
+
+class PostDisplay(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+
+class PostComment(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = 'post_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(PostComment, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse('post_detail', kwargs={'pk': post.pk}) + '#comments'
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = PostDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PostComment.as_view()
+        return view(request, *args, **kwargs)
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
